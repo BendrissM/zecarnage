2cb42f2e0271ae92ea593bd2c839bb905cebfbb9
Moncef Bendriss <moncef-2006@live.fr>
2018-02-04 00:25:52 +0000

    change modal header

--- resources/views/modals/delete_modal.blade.php
+++ resources/views/modals/delete_modal.blade.php
@@ -5,12 +5,12 @@
         <div class="modal-header" style="background-color: #dd4b39;">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" style="color: white;">Suprimer Blog</h4>
+          <h4 class="modal-title" style="color: white;">Supprimer l'article</h4>
         </div>
         {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'id' => 'deleteForm']) !!}
         <div class="modal-body">
             {{ csrf_field() }}
-            veux-tu vraiment supprimer ce blog {{$post->id}} ?
+            veux-tu vraiment supprimer l'article : <b><i>{{$post->title}}</i></b> ?
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
